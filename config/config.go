e5a8f5b8633019af1f2723ea0400f7978e7eee63
kyleochata <kyleochata@gmail.com>
2024-11-06 10:07:11 -0800

    edits

--- config/config.go
+++ config/config.go
@@ -2,11 +2,17 @@ package config
 
 import (
 	"log"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 func InitConfig(fileName string) *viper.Viper {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 	config := viper.New()
 	config.SetConfigName(fileName)
 	config.AddConfigPath(".")
@@ -15,5 +21,16 @@ func InitConfig(fileName string) *viper.Viper {
 	if err != nil {
 		log.Fatal("Error while parsing configuration file", err)
 	}
+	replaceEnvVariables(config)
 	return config
+}
+
+func replaceEnvVariables(config *viper.Viper) {
+	for _, key := range config.AllKeys() {
+		value := config.GetString(key)
+		if strings.Contains(value, "${") {
+			newValue := os.ExpandEnv(value)
+			config.Set(key, newValue)
+		}
+	}
 }
